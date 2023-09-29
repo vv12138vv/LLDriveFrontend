caf76e65aff22429cb4d11db362da6c1aa6ec8a6
vv12138vv <jgss987611676@outlook.com>
2023-09-29 22:44:21 +0800

    delete some bad code

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -228,16 +228,6 @@ defineExpose({
   reload,
 });
 
-// const api = {
-//   loadDataList: "/file/loadDataList",
-//   rename: "/file/rename",
-//   newFoloder: "/file/newFoloder",
-//   getFolderInfo: "/file/getFolderInfo",
-//   delFile: "/file/delFile",
-//   changeFileFolder: "/file/changeFileFolder",
-//   createDownloadUrl: "/file/createDownloadUrl",
-//   download: "/api/file/download",
-// };
 
 const fileAccept = computed(() => {
   const categoryItem = CategoryInfo[category.value];
